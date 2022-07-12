d33100f1e4b72388f9dbdf6a7ddf5f5605e31fb0
overtae <rlaxodud911@gmail.com>
2022-07-12 23:23:33 +0900

    Feat: 홈 페이지 html 코드 작성

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,35 @@
 const Home = () => {
-  return <div>홈</div>;
+  return (
+    <div>
+      {/* Navigation */}
+      <div>코멘토 쇼핑</div>
+      {/* ThemeButton */}
+      <section>
+        <div>#겨울_방한템</div>
+        <div>#따순머그컵</div>
+      </section>
+      {/* ProductCard */}
+      <section>
+        <div>
+          <img src="https://cdn.pixabay.com/photo/2015/11/23/08/22/tea-time-1057880_1280.jpg" alt="머그컵" />
+          <div>비숑 머그컵</div>
+          <div>쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.</div>
+        </div>
+
+        <div>
+          <img src="https://cdn.pixabay.com/photo/2015/11/23/08/22/tea-time-1057880_1280.jpg" alt="머그컵" />
+          <div>비숑 머그컵</div>
+          <div>쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.</div>
+        </div>
+
+        <div>
+          <img src="https://cdn.pixabay.com/photo/2015/11/23/08/22/tea-time-1057880_1280.jpg" alt="머그컵" />
+          <div>비숑 머그컵</div>
+          <div>쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.</div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
