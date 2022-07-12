4afae7e9bf467fe7612a91d85c02d48c4b85ff72
overtae <rlaxodud911@gmail.com>
2022-07-12 23:16:37 +0900

    Feat: 기본 라우팅 및 라우팅 용 임시 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Basket from "./pages/Basket";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter basename="comento-shopping">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
