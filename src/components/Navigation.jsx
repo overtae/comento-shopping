37b27e90bc643b0392fe7b1e4fe64363c2755f7a
overtae <rlaxodud911@gmail.com>
2022-07-13 01:09:39 +0900

    Design: Navigation 컴포넌트 디자인 적용

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,22 @@
+import styled from "styled-components";
+
 const Navigation = () => {
   return (
-    <div>
+    <NavigationStyled>
       <div>코멘토 쇼핑</div>
-    </div>
+    </NavigationStyled>
   );
 };
+
+const NavigationStyled = styled.div`
+  padding: 20px 151px;
+  border-bottom: 1px solid rgba(238, 238, 238, 1);
+
+  /* Font Style */
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 16.06px;
+  text-align: center;
+`;
 
 export default Navigation;
