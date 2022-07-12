69babc3a296baf4dc41f4ca9b98bfe3fc5536354
overtae <rlaxodud911@gmail.com>
2022-07-13 04:04:53 +0900

    Design: 상품 상세 페이지 컴포넌트들 디자인

--- src/components/BottomButton.jsx
+++ src/components/BottomButton.jsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import { SemiBoldText } from "../GlobalTextStyle";
+
 const BottomButton = ({ text }) => {
-  return <div>{text}</div>;
+  return <BottomButtonStyled>{text}</BottomButtonStyled>;
 };
+
+const BottomButtonStyled = styled.div`
+  background: #24dbaf;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 390px;
+  height: 70px;
+  cursor: pointer;
+  ${SemiBoldText}
+`;
 
 export default BottomButton;
--- src/components/ProductDetailImage.jsx
+++ src/components/ProductDetailImage.jsx
@@ -1,9 +1,17 @@
+import styled from "styled-components";
+
 const ProductDetailImage = ({ image }) => {
   return (
-    <div>
+    <ImageBox>
       <img src={image} alt="상품 설명" />
-    </div>
+    </ImageBox>
   );
 };
+
+const ImageBox = styled.div`
+  padding: 24px;
+  width: 342px;
+  height: 1200px;
+`;
 
 export default ProductDetailImage;
