c51a09a18fc0b6e7f6a61359f77502b467b1cd29
overtae <rlaxodud911@gmail.com>
2022-07-20 21:17:18 +0900

    Fix: ProductCard에 onClick 이벤트 추가

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
-const ProductCard = ({ name, description, thumbnail }) => {
+const ProductCard = ({ name, description, thumbnail, onClick }) => {
   return (
-    <ProductCardStyled>
+    <ProductCardStyled onClick={onClick}>
       <Thumbnail src={thumbnail} alt={name} />
       <Name>{name}</Name>
       <Description>{description}</Description>
