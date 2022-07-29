28a64043951933c1997b6a96f85916b0e5178e45
overtae <rlaxodud911@gmail.com>
2022-07-30 05:44:02 +0900

    Feat: 상품 금액, 배송비, 주문 금액 총합 구현

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -4,10 +4,16 @@ import Navigation from "../components/Navigation";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import * as storage from "../utils/storage";
+import BasketPrice from "../components/BasketPrice";
+import styled from "styled-components";
+
+const DELIVERYFEE = 3000;
 
 const Basket = () => {
   const [basketItems, setBasketItems] = useState();
   const [basketItemCount, setBasketItemCount] = useState(0);
+  const discountPrice = null;
+  let [totalPrice, setTotalPrice] = useState(0);
   let navigate = useNavigate();
 
   useEffect(() => {
@@ -18,32 +24,42 @@ const Basket = () => {
 
   useEffect(() => {
     const items = storage.getBasketItems();
+    const total = items.reduce((prev, current) => prev + current.price, 0);
     setBasketItems(items);
+    setTotalPrice(total);
   }, [basketItemCount]);
 
   const onClickRemoveButton = (productId) => {
     storage.removeBasketItem(productId);
     setBasketItemCount(basketItemCount - 1);
   };
+
   return (
     <div>
       <Navigation text="장바구니" goBack={() => navigate(-1)} />
 
-      {basketItems &&
-        basketItems.map((item) => (
-          <BasketProduct
-            key={item.id}
-            thumbnail={item.thumbnail}
-            name={item.name}
-            price={item.price}
-            onClickRemoveButton={() => onClickRemoveButton(item.id)}
-          />
-        ))}
+      <BasketItemsStyled>
+        {basketItems &&
+          basketItems.map((item) => (
+            <BasketProduct
+              key={item.id}
+              thumbnail={item.thumbnail}
+              name={item.name}
+              price={item.price}
+              onClickRemoveButton={() => onClickRemoveButton(item.id)}
+            />
+          ))}
+      </BasketItemsStyled>
 
-      <div>상품 금액({basketItemCount} 개)</div>
+      <BasketPrice itemCount={basketItemCount} totalPrice={totalPrice} discountPrice={discountPrice} deliveryFee={DELIVERYFEE} />
+
       <BottomButton text="주문하기" />
     </div>
   );
 };
+
+const BasketItemsStyled = styled.div`
+  padding-bottom: calc(70px + 140px);
+`;
 
 export default Basket;
